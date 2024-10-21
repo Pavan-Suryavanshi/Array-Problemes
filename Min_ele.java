684968476d2da320622a88dad103452e5399dc5f
Pavan-Suryavanshi <pavansuryavanshi52@gmail.com>
2024-10-21 12:27:45 +0530

    min ele code

--- Min_ele.java
+++ Min_ele.java
@@ -1,1 +1,21 @@
-Sysojennfkawjnfkasnckasjanv;ksanfkwae
+/**
+ * Min_ele
+ */
+public class Min_ele {
+
+    public static void main(String[] args) {
+        int[] a={1,4,2,5,7,5,8,9};
+        int min=a[0];
+        for(int i=0;i<a.length;i++)
+        {
+            if(min>a[i])
+            {
+                min=a[i];
+            }
+            
+        }
+        System.out.println(min);
+
+
+    }
+}
